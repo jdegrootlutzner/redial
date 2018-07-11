14b6fcd12a290ff58c397127eef43817c583710f
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2018-07-11 15:56:33 +0000

    Dev mode seq, stories on csv

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import os
 import time
+import csv
 import random
 import subprocess
 import RPi.GPIO as GPIO
+from google.cloud import storage
+import wave
 
 """
 @author Julian DeGroot-Lutzner
 @date Summer 2018
 
 """
-
-GPIO.setwarnings( True )
-GPIO.setmode( GPIO.BCM )
 
 # GPIO output numbers
 COUNTER_PIN = 2 # on the rotaty, blue and green wires
@@ -20,23 +20,49 @@ LEVER_PIN = 27 # lever that phone sits on, on when down, off when up
 
 SOUND_CARD = 1 # location of usb sound card
 
-# set up GPIOs 
-GPIO.setup( COUNTER_PIN , GPIO.IN , pull_up_down = GPIO.PUD_DOWN )
-GPIO.setup( DIALING_PIN , GPIO.IN , pull_up_down = GPIO.PUD_DOWN )
-GPIO.setup( LEVER_PIN   , GPIO.IN , pull_up_down = GPIO.PUD_UP)
+# Google Cloud parameters
+BUCKET_NAME = 'telephone-project'
+GOOGLE_APPLICATION_CREDENTIALS = '"/home/pi/Desktop/telephone-project/not-for-git/google-credentials.json"'
+
+EASTER_EGG = ('Poopy-di scoop. Scoop-ditty-whoop. '
+              'Whoop-di-scoop-di-poop. Poop-di-scoopty. '
+              'Scoopty-whoop.'
+             )
 
 # global variables
 c = 0 # c is the count on the rotary phone
-busy = False # when true, user should be prevented from interacting
+busy = False # when true, user should be prvntd frm actn excpt for spc_action
+special_c = 0 # a count on the rotary that is used within a spc_action
+attempting_dev_mode = False # user attempting to enter developer mode
+
+# set up GPIOs 
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(COUNTER_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN )
+GPIO.setup(DIALING_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN )
+GPIO.setup(LEVER_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+# set up google-cloud-storage
+os.system('export GOOGLE_APPLICATION_CREDENTIALS=%s' % GOOGLE_APPLICATION_CREDENTIALS)
+client = storage.Client()
+bucket = client.get_bucket(BUCKET_NAME)
+blob = bucket.get_blob('test/test.txt')
+print(blob.download_as_string())
+
+
+# import list of location of the 9 stories on the phone
+input_file = open('story-info.csv', 'r')
+row = next(csv.reader(input_file))
+input_file.close()
 
 
 def print_statuses():
     """ Used for debugging, print the count and the status of the 
     different input pins """
-    print("  c = " + str(c))
-    print( "  counter pin = " + str( GPIO.input( COUNTER_PIN ) ) )
-    print( "  dialing pin = " + str( GPIO.input( DIALING_PIN ) ) )
-    print( "  lever pin = " + str( GPIO.input( LEVER_PIN ) ) ) 
+    print('  c = ' + str(c))
+    print('  counter pin = ' + str(GPIO.input(COUNTER_PIN)))
+    print('  dialing pin = ' + str(GPIO.input(DIALING_PIN)))
+    print('  lever pin = ' + str(GPIO.input(LEVER_PIN))) 
 
 def text_to_speech(text):
     """
@@ -45,10 +71,10 @@ def text_to_speech(text):
     cmd = 'espeak -s110 "%s" --stdout | aplay -D sysdefault:CARD=%s' % (text, SOUND_CARD)
     os.system(cmd)
 
-text_to_speech("Hello. My name is Julian. I work at the Library Innovation Lab. I like to run.:")
+text_to_speech('Hello.')
 
 def play(filename):
-    print("getting filename", filename)
+    print('getting filename', filename)
     code = os.system('aplay --device=plughw:0,0 %s' % filename)
     return code
 
@@ -59,10 +85,19 @@ def record(filename):
 
 
 def play_story( story_number ):
-    """ """
-    # 
+    '''
+    When someone records a new story:
+    - store the story on google cloud storage
+        - write the url to reactive csv file
+            http://{BUCKET_NAME}.storage.googleapis.com/{FILE_NAME}
+                - {BUCKET_NAME} = 'telephone-project' in this case
+                - {FILE_NAME} = 'name of story/ iteration of story'
+        - write 'Transcription in process' to the reactive csv file
+    - transcribe the story
+        - write trancription to a reactive csv file
+    '''
 
-def easter_egg():
+def developer_mode():
     """ If the user dials a number larger than 10, play a special story """
 
 
@@ -70,62 +105,101 @@ def main( pin ):
     """ Function called when dial returns to resting state. If the phone is 
     off the receiver and no other story is being played, then play a new story
     """
-    global c, busy
-    if busy or not GPIO.input( LEVER_PIN ):
-        print( "busy = " + str(busy) +
-                "   and   lever = " + str( GPIO.input( LEVER_PIN )))
+    global c, busy, special_c, attempting_dev_mode
+    print('busy', busy)
+    if not GPIO.input(LEVER_PIN):
+        # If the phone is on the hook, do not allow anything to happen
+        print('Receiver lever down. You cannot use the rotary dial.')
+    elif busy:
+        # If an event is happening, check to see if we are in a special event
+        # if we are not in a special event, then do nothing
+        if attempting_dev_mode:
+            if special_c == 5:
+                print('Developer mode unlocked')
+            else:
+                print('Nothing')
+            attempting_dev_mode = False
+            busy = False
+        else:
+            print('Busy. You are already using the phone.')
+
     else:
+        # This is the case in which no event has been called yet. 
+        # Allow the user to dial a number and signal an event
         if c == 0:
             #do nothing 
-            print( "no number dialed" )
+            print('no number dialed')
         elif c >= 1 and c <= 10 :
             busy = True
-            print( "playing story : " + str(c) )
+            print('playing story : ' + str(c))
             # play_story( c )
-            if GPIO.event_detected( COUNTER_PIN ):
-                print( "number dialed" )
+            if GPIO.event_detected(COUNTER_PIN):
+                print('number dialed')
 
 
-            # wait until easter egg is over then set busy to false
+            # wait until event is over then set busy to false
             busy = False # temp, may be better to feed in bool to story call
         elif c > 10: 
             busy = True
-            print( "easter egg")
-            # wait until easter egg is over then set busy to false
-            busy = False # temp, may be better to feed in bool to story call
-        print("resetting")
+            print('easter egg')
+            #text_to_speech(EASTER_EGG)
+            attempting_dev_mode = True
+            #YOU ARE HERE - FIGURING OUT LOCK ________ 
+        print('resetting')
         c = 0
+        special_c = 0
 
-
-def count( pin ):
-    global c, busy
-    if busy or not GPIO.input( LEVER_PIN ):
-        print( "busy = " + str( busy ) 
-                + "   and   lever = " + str( GPIO.input( LEVER_PIN )))
+def count(pin):
+    global c, busy, special_c, special_action
+    if (not GPIO.input(LEVER_PIN)):
+        print('Receiver lever down. You cannot use the rotary dial.')
+    elif busy:
+        if attempting_dev_mode:
+            special_c = special_c + 1
+            print('special_c', special_c)
+        else:
+            print('Busy. You are already using the phone.')
     else:
         c = c + 1
-        print( "counting" , c )
+        print('counting' , c)
 
 #GPIO.add_event_detect( DIALING_PIN , GPIO.BOTH , callback = main )
-GPIO.add_event_detect( COUNTER_PIN , GPIO.FALLING , callback=count , 
-                                                    bouncetime=85 )
+GPIO.add_event_detect(COUNTER_PIN, GPIO.FALLING, callback=count, 
+                                                    bouncetime=85)
 
-GPIO.add_event_detect( DIALING_PIN , GPIO.RISING , callback = main )
+GPIO.add_event_detect(DIALING_PIN, GPIO.RISING, callback = main)
 
 
 
 def cleanup():
-    GPIO.remove_event_detect( COUNTER_PIN )
-    GPIO.remove_event_detect( DIALING_PIN )
+    # Close GPIO connections
+    GPIO.remove_event_detect(COUNTER_PIN)
+    GPIO.remove_event_detect(DIALING_PIN)
     GPIO.cleanup()
+
+    # Write story locations to a csv file 
+    output_file = open('story-info.csv.tmp', 'w')
+    csv.writer(output_file).writerow(row)
+    output_file.close()
+    # move tmp file to original csv file
+    os.system('mv story-info.csv.tmp story-info.csv')       
 
 try:
     while True:
-        time.sleep( 50000 )
+        time.sleep(50000)
 except KeyboardInterrupt:
     cleanup()
-    print( 'goodbye!' )
+    print('goodbye!')
     exit()
+
+'''
+tags$audio(src = "telephone-project.storage.googleapis.com/prototype-story/1.wav", type = "audio/mp3", autoplay = NA, controls = NA)
+
+"<HTML>
+<audio src="telephone-project.storage.googleapis.com/prototype-story/1.wav" type="audio/wav" autoplay controls></audio>
+</HTML>
+"
+'''
 
 cleanup()
 exit()
